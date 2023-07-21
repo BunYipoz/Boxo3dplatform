6adaa10d033197a0258141b33d3bd279d1e1dd36
Firefox <firefox@westnet.com.au>
2023-07-21 22:11:42 +1000

    initial comment

---
+++ Assets/Code/Char.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Char : MonoBehaviour
+{
+    public float Speed = 5f;
+    public float SSpeed = 10f;
+    public float Jump = 7f;
+    public float hurt = 600f;
+    public bool gc = false;
+    public bool GroundTouch = false;
+    public Text countText;
+    public Text winText;
+    private int count;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gc = true;
+        count = 0;
+        SetCountText();
+    }
+
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.LeftShift))
+        {
+            transform.position += Vector3.forward * Time.deltaTime * SSpeed;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.forward * Time.deltaTime * Speed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.back * Time.deltaTime * Speed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * Time.deltaTime * Speed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * Time.deltaTime * Speed;
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.position += Vector3.up * Time.deltaTime * Jump;
+        }
+        Vector3 gc = transform.TransformDirection(Vector3.down);
+        if (Physics.Raycast(transform.position, gc, .5f))
+        {
+            GroundTouch = true;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive (false);
+            count = count + 1;
+            SetCountText();
+        }
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            transform.position += Vector3.back * Time.deltaTime * hurt;
+            count = count - 1;
+            SetCountText ();
+            if (count <= -1)
+            {
+                Destroy (gameObject);
+                countText.text = "";
+                winText.text = "You lose :(";
+            }
+        }
+        if (other.gameObject.CompareTag("Finish"))
+            {
+                countText.text = "";
+                winText.text = "Congrats you win ! ! !";
+            }
+        
+    }
+    void SetCountText ()
+    {
+        countText.text = "Count: " + count.ToString ();
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
